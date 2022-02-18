d4ee7f0e906d1f115aefb7d533043df77e720f8f
godwin-pk <80232410+godwin-pk@users.noreply.github.com>
2022-02-18 16:17:33 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,12 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    AccountEmail              = "US_AWS-Explore+Landing-Zone-Accelerator@pwc.com"
+    AccountName               = "PwC Explore - Landing Zone Accelerator"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "godwin.p.k@pwc.com"
+    SSOUserFirstName          = "Godwin"
+    SSOUserLastName           = "K"
   }
 
   account_tags = {
@@ -23,5 +23,5 @@ module "sandbox" {
     group = "non-prod"
   }
 
-  account_customizations_name = "sandbox"
+  account_customizations_name = "sandbox_test"
 }
